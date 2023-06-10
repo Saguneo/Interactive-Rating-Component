bd90aeb6e335e2e4658d4a955f7ad677ca39512e
Saguneo <88016255+Saguneo@users.noreply.github.com>
2023-06-10 18:32:08 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const firstContainer = document.querySelector(".first-content");
 const secondContainer = document.querySelector(".second-content");
-const ratingOptions = document.querySelectorAll(".rating-button");
+const ratingOptions = document.querySelectorAll(".radio-label");
 const submitButton = document.getElementById("submit-button");
 const finalRating = document.getElementById("customer-rating");
 
@@ -12,12 +12,5 @@ submitButton.addEventListener("click", () => {
 ratingOptions.forEach((options) => {
   options.addEventListener("click", () => {
     finalRating.innerHTML = options.innerHTML;
-  });
-});
-
-ratingOptions.forEach((option) => {
-  option.addEventListener("click", () => {
-    ratingOptions.forEach((el) => el.classList.remove("selected"));
-    option.classList.add("selected");
   });
 });
